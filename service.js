2a9c3c9a21b2a5e5db4a30c98370b668eb61984c
Alfonso Ríos <manuelalfonso.rios.medellin.next@bbva.com>
2019-09-13 11:19:08 -0500

    Add logs of the body and headers

--- service.js
+++ service.js
@@ -4,6 +4,8 @@ const kueskiConfirmation = (request, response) => {
   const payload = request.body;
   response.set('Content-Type', 'application/json');
   response.set('Authorization', 'Bearer 5ccd922a-e0e4-4d38-a936-1d22d3b70d40');
+  console.log(JSON.stringify(request.body));
+  console.log('headers', JSON.stringify(request.headers));
   switch (payload.status) {
     case 'approved':
       response.status(200).json({
