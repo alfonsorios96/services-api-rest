4ee30a722c473d8ecfd901484b53cf4934bfd6d5
Alfonso Ríos <manuelalfonso.rios.medellin.next@bbva.com>
2019-09-23 18:14:31 -0500

    Change secret key

--- service.js
+++ service.js
@@ -3,7 +3,7 @@
 const kueskiConfirmation = (request, response) => {
   const payload = request.body;
   response.set('Content-Type', 'application/json');
-  response.set('Authorization', 'Bearer 5ccd922a-e0e4-4d38-a936-1d22d3b70d40');
+  response.set('Authorization', 'Bearer bef6fc94-a772-4ed6-bcd9-0796e9f13f3f');
   console.log(JSON.stringify(request.body));
   console.log('headers', JSON.stringify(request.headers));
   switch (payload.status) {
